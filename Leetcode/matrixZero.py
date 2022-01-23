6cadc91e5cbfe30a248480808956ef4859c88377
Zakir Dawood <71907707+zakirdawood@users.noreply.github.com>
2022-01-23 13:56:16 -0500

    Update matrixZero.py

--- Leetcode/matrixZero.py
+++ Leetcode/matrixZero.py
@@ -1,28 +1,22 @@
-class Solution(object):
-    def sweeper(self, matrix, row, col):
-        for i in range(len(matrix[row])):
-            if matrix[row][i] != 0:
-                matrix[row][i] = 2147483648
-        
-        for i in range(len(matrix)):
-            if matrix[i][col] !=0:
-                matrix[i][col] = 2147483648
-            
+class Solution(object): 
     def setZeroes(self, matrix):
-        iVar = 0
-        jVar = 0
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                print(matrix[i][j])
-                if matrix[i][j] == 0:
-                    self.sweeper(matrix, iVar, jVar)
-                jVar += 1
-            
-            iVar += 1
-            jVar = 0
-            
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if matrix[i][j] == 2147483648:
-                    matrix[i][j] = 0
+        for x in range(len(matrix)):
+            for y in range(len(matrix[x])):
+                if matrix[x][y] == 0:
+                    for z in range(len(matrix)):
+                        if matrix[z][y] != 0:
+                            matrix[z][y] = 2147483648
+                    for z in range(len(matrix[x])):
+                        if matrix[x][z] != 0:
+                            matrix[x][z] = 2147483648
+                        
+        for x in range(len(matrix)):
+            for y in range(len(matrix[x])):
+                if matrix[x][y] == 2147483648:
+                    matrix[x][y] = 0
                     
+#---------------Solution Stats---------------#
+#         Test Cases Passed: 164/164
+#Runtime: 121ms (Top 55% of Python Submissions)
+#Memory Usage: 14.2MB (Top 40% of Python Submissions)
+#--------------------------------------------#                    
