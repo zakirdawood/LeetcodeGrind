ed572a93a234782e813c84d659868a9e9d5a69d8
Zakir Dawood <71907707+zakirdawood@users.noreply.github.com>
2022-01-23 11:56:23 -0500

    Update triplets.py

--- Leetcode/triplets.py
+++ Leetcode/triplets.py
@@ -4,26 +4,36 @@ class Solution(object):
         nums.sort()
         n = len(nums)
         
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            y = i + 1
-            z = n - 1
-            
-            while y < z:
-                total = nums[i] + nums[y] + nums[z]
-                if total < 0:
-                    y += 1
-                elif total > 0:
-                    z -= 1
-                else:
-                    triplets.append([nums[i], nums[y], nums[z]])
-                    while y < z and nums[y] == nums[y+1]:
+        if n > 3:
+            for i in range(n - 2):
+                if i > 0 and nums[i] == nums[i-1]:
+                    continue
+                    
+                y = i + 1
+                z = n - 1
+
+                while y < z:
+                    total = nums[i] + nums[y] + nums[z]
+                    if total < 0:
                         y += 1
-                    while y < z and nums[z] == nums[z-1]:
+                    elif total > 0:
                         z -= 1
-                    y += 1
-                    z -= 1
-        
+                    else:
+                        triplets.append([nums[i], nums[y], nums[z]])
+                        while y < z and nums[y] == nums[y+1]:
+                            y += 1
+                        while y < z and nums[z] == nums[z-1]:
+                            z -= 1
+                        y += 1
+                        z -= 1
+        elif n == 3:
+            if nums[0] + nums[1] + nums[2] == 0:
+                triplets.append(nums)
+
         return triplets
 
+#---------------Solution Stats---------------#
+#        Test Cases Passed: 318/318
+#Runtime: 604ms (Top 10% of Python Submissions)
+#Memory Usage: 16.7MB (Top 30% of Python Submissions)
+#--------------------------------------------#
