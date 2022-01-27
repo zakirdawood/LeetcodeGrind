434fd82d0f5aad16f6087b6601041ba68439783f
Zakir Dawood <71907707+zakirdawood@users.noreply.github.com>
2022-01-26 19:00:10 -0500

    Add files via upload

---
+++ Leetcode/longeststringOn.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        start, end, num = 0, 0, 0
+        repeat = set()
+        strLen = len(s)
+        
+        while end < strLen:
+            if s[end] in repeat:
+                repeat.clear()
+                start += 1
+                end = start
+                if (strLen - start) < num:
+                    break
+            else:
+                repeat.add(s[end])
+                end += 1
+                if (end - start) > num:
+                    num = end - start
+        return num
