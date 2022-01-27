14d5c9bcc53c6478feda32f4111530e2ce33228a
Zakir Dawood <71907707+zakirdawood@users.noreply.github.com>
2022-01-26 19:01:14 -0500

    Update longeststringOn.py

--- Leetcode/longeststringOn.py
+++ Leetcode/longeststringOn.py
@@ -17,3 +17,9 @@ class Solution(object):
                 if (end - start) > num:
                     num = end - start
         return num
+    
+#---------------Solution Stats---------------#
+#        Test Cases Passed: 987/987
+#Runtime: 600ms (Top 75% of Python Submissions)
+#Memory Usage: 13.6MB (Top 10% of Python Submissions)
+#--------------------------------------------#
